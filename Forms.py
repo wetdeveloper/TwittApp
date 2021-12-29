20c80360f5b6c927963b2741e296a77c8515d410
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2021-12-29 15:55:16 +0330

    Add files via upload

---
+++ Forms.py
@@ -0,0 +1,13 @@
+from app import *
+class SignupForm(FlaskForm):   
+      username = StringField('username', [validators.DataRequired(), validators.Length(max=64)])
+      password = PasswordField('password', [validators.DataRequired(),validators.Length(min=8)])
+      submit=SubmitField('Signup')
+      recaptcha = RecaptchaField()
+
+
+class LoginForm(FlaskForm):   
+      username = StringField('username', [validators.DataRequired(), validators.Length(max=64)])
+      password = PasswordField('password', [validators.DataRequired(),validators.Length(min=8)])
+      submit=SubmitField('Login')
+      recaptcha = RecaptchaField()
