36f0c9794ff0df50ce52cf3979232f5c4d869667
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2021-12-29 15:56:14 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import uuid
 from flask import Flask,render_template,url_for,request,redirect,jsonify
 from flask_login import login_user,LoginManager,current_user,logout_user
 from flask_sessionstore import Session
-from flask_wtf import Form ,CSRFProtect
+from flask_wtf import FlaskForm ,CSRFProtect,RecaptchaField
+from wtforms import PasswordField,StringField, validators,SubmitField
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from sqlalchemy.engine import Engine
@@ -14,12 +15,18 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_moment import Moment 
 
+
 app=Flask(__name__, template_folder='static')
 app.config['SECRET_KEY']='thisisehsanghgfhfdgd'
 app.config['WTF_CSRF_SECRET_KEY']='wtffdjfksdjfksjg;jgkdlsgh'
 
 login_manager=LoginManager()
 login_manager.init_app(app)
+
+app.config['RECAPTCHA_USE_SSL']= False
+app.config['RECAPTCHA_PUBLIC_KEY']= '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+app.config['RECAPTCHA_PRIVATE_KEY']='6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
 
 Bootstrap(app)
 db = SQLAlchemy(app)
@@ -44,3 +51,4 @@ moment=Moment(app)
 
 if __name__=='__main__':
     app.run(debug='True')
+
