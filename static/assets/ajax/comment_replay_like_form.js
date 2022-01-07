efea2f06189350a831c220bc69b1181942299c04
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-07 14:46:20 +0330

    Update comment_replay_like_form.js

--- static/assets/ajax/comment_replay_like_form.js
+++ static/assets/ajax/comment_replay_like_form.js
@@ -1,3 +1,4 @@
+
 $(document).ready((function(){
     console.log('ajax-comment_replay_like_form')
     $(document.getElementsByClassName('comment_replay_like_form_submit')).on('click',function(e){
@@ -6,25 +7,28 @@ $(document).ready((function(){
     var commentid=$(this).closest('.comment_replay_like_form').find("#commentid").val();
     var comment_replay_id=$(this).closest('.comment_replay_like_form').find("#comment_replay_id").val();
     var replaytable=$(this).closest('.comment_replay_like_form').find("#replaytable").val();
-    console.log(`twittid:${twittid} , commentid:${commentid} , comment_replay_id:${comment_replay_id} ,
-    replaytable:${replaytable}`)
-    $.ajax({
-        type:'post',
-        url:"/home/twitts/see_comments/like_comment_replays/", 
-        data:{
+    data={
         twittid:twittid,
         commentid:commentid,
         comment_replay_id:comment_replay_id,
         replaytable:replaytable
-        },
+        }
+    $.ajax({
+        type:'post',
+        url:"/home/twitts/see_comments/like_comment_replays/",
+        data:data ,
         success:function(response)
         {
-            document.getElementById('comment_replay_like_form_'+response.comment_replay_id).innerHTML="<h3 style='color: red;'><i class='glyphicon glyphicon-heart'></i</h3>"
-            var comment_replay_likes_number_tag=document.getElementById('comment_replay_likes_number_'+response.comment_replay_id)
-            comment_replay_likes_number_tag.innerHTML=response.comment_replay_likes_number
-            console.log(response.message)
-            console.log('comment_replay_id'+response.comment_replay_id)
-            console.log('comment replay likes number='+response.comment_replay_likes_number)
+            alert(response.message)
+            document.getElementById('comment_replay_likes_number_'+response.comment_replay_id).innerHTML=response.comment_replay_likes_number
+            if(response.unliked==false){
+                
+                $('#comment_replay_like_form_'+data.comment_replay_id).find('.comment_replay_like_form').find('.comment_replay_like_form_submit').html("<h3 style='color:red'><i class='glyphicon glyphicon-heart'></i></h3>")
+
+            }
+            else if (response.unliked==true){
+                $('#comment_replay_like_form_'+data.comment_replay_id).find('.comment_replay_like_form').find('.comment_replay_like_form_submit').html("<h3><i class='glyphicon glyphicon-heart-empty'></i></h3>")
+            }
         }
     })
     });
