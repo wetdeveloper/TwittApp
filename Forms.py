cba769368b3e0e739590bd83a61c16692c6b08ed
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-06-24 00:16:31 +0430

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -2,12 +2,24 @@ from app import *
 class SignupForm(FlaskForm):   
       username = StringField('username', [validators.DataRequired(), validators.Length(max=64)])
       password = PasswordField('password', [validators.DataRequired(),validators.Length(min=8)])
+      email = StringField("Email",  validators=[validators.DataRequired()])
       submit=SubmitField('Signup')
-      #recaptcha = RecaptchaField()
+      # recaptcha = RecaptchaField()
 
 
 class LoginForm(FlaskForm):   
       username = StringField('username', [validators.DataRequired(), validators.Length(max=64)])
       password = PasswordField('password', [validators.DataRequired(),validators.Length(min=8)])
       submit=SubmitField('Login')
-      #recaptcha = RecaptchaField()
+      # recaptcha = RecaptchaField()
+
+class ForgetPasswordForm(FlaskForm):   
+      username = StringField('username', [validators.DataRequired(), validators.Length(max=64)])
+      submit=SubmitField('Send Verification Code')
+      # recaptcha = RecaptchaField()
+
+class ResetPasswordForm(FlaskForm):
+      newPassword=StringField('New password',[validators.DataRequired(), validators.Length(max=64,min=8)])
+      verificationCode=StringField('Verification code',[validators.DataRequired(), validators.Length(max=4,min=4)])
+      submit=SubmitField('Rest the password')
+      # recaptcha = RecaptchaField()
